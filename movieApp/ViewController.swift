2e4d55df7bb000978a44ed2ab11af36ab88c4587
Carlos Torres Sanchez <ing.torres.carlos@gmail.com>
2019-06-26 21:00:52 -0500

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Carlos Torres Sanchez on 6/26/19.
+//  Copyright © 2019 Carlos Torres Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
